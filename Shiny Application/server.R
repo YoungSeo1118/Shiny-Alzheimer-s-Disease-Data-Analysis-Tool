baa0f732150955c0866ee5a57bbc613c520080b3
Younghoon Seo <80616847+YoungSeo1118@users.noreply.github.com>
2021-10-24 13:08:59 +0900

    Update server.R

--- Shiny Application/server.R
+++ Shiny Application/server.R
@@ -1,15 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-setwd("/Users/macintosh/Desktop/R_App/Data_Analysis_App/")
-
-
 library(shiny)
 source("lesson_7.R")
 
@@ -29,9 +17,7 @@ shinyServer(function(input,output){
                "APOE4" = list("APOE4"),
                "DX, APOE4" = list("DX","APOE4"))
     })
-    
-    # covariate<-eventReactive(input$run_button,covariate())
-    
+        
     table<-eventReactive(input$run_button,{
         htmlTable(wrapper_function(independent(),dependent(),covariate())[[1]])
     })
